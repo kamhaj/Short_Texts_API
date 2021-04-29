66ed20086955783b918a209353071c437b8ac99b
Kamil <ckkomancz18@yahoo.com>
2021-04-29 18:11:12 +0200

    new secret key generated (old one removed from settings.py)

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from secret_settings import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g$f#e#7u*p!3&q64bcxv*f#o5ahyg^13^cxxk((as3^g95^qkp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
