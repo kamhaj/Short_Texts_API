6f243cb1f161688aac1e68e8ea70b8241462a282
Kamil <ckkomancz18@yahoo.com>
2021-05-01 14:07:07 +0200

    update url (post details) and DEBUG to env. var.

--- blog/settings.py
+++ blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DJANGO_DEBUG', True)
 ALLOWED_HOSTS = ['kamhaj-short-texts.herokuapp.com',
                 'localhost',
                 '127.0.0.1']
