debba26de1e8feff805c22ff0bec4522d9813f2b
kamhaj <35563504+kamhaj@users.noreply.github.com>
2021-05-02 15:30:49 +0200

    Update models.py

--- short_texts/models.py
+++ short_texts/models.py
@@ -1,9 +1,7 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import User
 
 
-## one (user) to many (posts) relation
 class Post(models.Model):
     title = models.CharField(max_length=60)
     content = models.CharField(max_length=160)       # unrestricted text
@@ -11,25 +9,23 @@ class Post(models.Model):
     edition_date = models.DateTimeField(blank=True, null=True)
     views_counter = models.IntegerField(default=0, null=True, blank=True)
 
-    ## change publish date (e.g. on update)
+    
+    ## change edition date (e.g. on update)
     def update_edition_date(self):
         self.updated_date = timezone.now()
         self.save()
 
-    ## update views counter by 1 each time it's being requested
+    ## update views counter by 1
     def update_views_counter(self):
         self.views_counter = self.views_counter + 1
         self.save()
 
-    ## update views counter by 1 each time it's being requested
+    ## reset views counter to 0
     def reset_views_counter(self):
         self.views_counter = 0
         self.save()
 
-    ## show short version of a post
-    def snippet(self):
-        return self.content[:50] + '...'
-
+        
     ## return it in a way you want it to be printed out   
     def __str__(self):
         return self.title
